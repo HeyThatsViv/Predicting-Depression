c5f990e412f198af8936e4f1b006c8e22cfc4f3e
Vivienne <viviennedifrancesco@gmail.com>
2021-01-16 21:27:03 -0700

    Saving progress

--- project_functions/__init__.py
+++ project_functions/__init__.py
@@ -11,6 +11,7 @@ import seaborn as sns
 def glob_concat(path, file_str):
     '''
     Looks for files in folder path and combines them into a DataFrame.
+    Also sets the index to be the SEQN column.
     
     Args:
         path (str): Location on computer where files are located.
@@ -18,7 +19,7 @@ def glob_concat(path, file_str):
         
     Returns:
         Displays the files that were found for verification and returns the 
-        DataFrame of all files after pd.concat is performed.
+        DataFrame of all files after pd.concat is performed and the index is set.
     
     Example:
         combined_df = glob_concat(r'File/File', '*.XPT')
@@ -28,6 +29,8 @@ def glob_concat(path, file_str):
     display(files)
     df_files = [pd.read_sas(file) for file in files]
     combined_df = pd.concat(df_files)
+    combined_df.SEQN = combined_df.SEQN.astype('int64')
+    combined_df.set_index('SEQN', verify_integrity=True, inplace=True)
     return combined_df
 
 
@@ -70,3 +73,69 @@ def cols_tokeep(df, col_list):
         else:
             pass
     return df_copy
+
+
+def first_cancer_count(x):
+    '''
+    Function for creating a new column that is a count from the first cancer
+    information of people that have had a first cancer.
+    
+    Args:
+        x(row in DataFrame): Pass over each entry in DataFrame to count if a
+            first cancer is recorded.
+        
+    Returns:
+        1 if information of a first cancer exists and 0 if it does not.
+    
+    Example:
+        df.apply(first_cancer_count, axis=1)
+    '''
+
+    if x['first_cancer_type'] != 'None':
+        return 1
+    else:
+        return 0
+
+
+def second_cancer_count(x):
+    '''
+    Function for creating a new column that is a count from the second cancer
+    information of people that have had a second cancer.
+    
+    Args:
+        x(row in DataFrame): Pass over each entry in DataFrame to count if a
+            second cancer is recorded.
+        
+    Returns:
+        1 if information of a second cancer exists and 0 if it does not.
+    
+    Example:
+        df.apply(second_cancer_count, axis=1)
+    '''
+
+    if x['second_cancer_type'] != 'None':
+        return 1
+    else:
+        return 0
+
+
+def third_cancer_count(x):
+    '''
+    Function for creating a new column that is a count from the third cancer
+    information of people that have had a third cancer.
+    
+    Args:
+        x(row in DataFrame): Pass over each entry in DataFrame to count if a
+            third cancer is recorded.
+        
+    Returns:
+        1 if information of a third cancer exists and 0 if it does not.
+    
+    Example:
+        df.apply(third_cancer_count, axis=1)
+    '''
+
+    if x['third_cancer_type'] != 'None':
+        return 1
+    else:
+        return 0
